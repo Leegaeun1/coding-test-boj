aeb4687d7ec90eb6ce6390bc3a48e3c1a7e70751
Leegaeun1 <140165122+Leegaeun1@users.noreply.github.com>
2025-09-09 13:44:09 +0900

    [Silver IV] Title: 동전 0, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/11047. 동전 0/동전 0.py
@@ -0,0 +1,13 @@
+n,k = list(map(int,input().split()))
+li = []
+cnt = 0
+
+for i in range(n):
+    li.append(int(input()))
+
+for i in range(n-1,-1,-1):
+    if li[i] <= k:
+        cnt += k //li[i]
+        k %= li[i]
+
+print(cnt)
