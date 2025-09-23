69782a9c38c13f7b1ad92d448546ca8271dc3eb6
Leegaeun1 <140165122+Leegaeun1@users.noreply.github.com>
2025-09-23 14:39:52 +0900

    [Silver I] Title: 숨바꼭질, Time: 288 ms, Memory: 79780 KB -BaekjoonHub

---
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
@@ -0,0 +1,25 @@
+from collections import deque
+
+n, k = map(int,input().split())
+visited = set()
+
+def bfs(n):
+    queue = deque([(n,0)])
+    visited.add(n)
+
+    while queue:
+        x, cnt = queue.popleft()
+        
+        if x == k:
+            return cnt
+        if x*2 not in visited and x < k:
+            queue.append((x*2,cnt+1))
+            visited.add((x*2))
+        if x-1 not in visited:
+            queue.append((x-1,cnt+1))
+            visited.add(x-1)
+        if x+1 not in visited and x < k:
+            queue.append((x+1,cnt+1))
+            visited.add(x+1)
+
+print(bfs(n))
