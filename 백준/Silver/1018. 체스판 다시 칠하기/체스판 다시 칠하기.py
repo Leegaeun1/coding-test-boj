5888c55c6dd4cc49a449c96e30727fa17710e737
Leegaeun1 <140165122+Leegaeun1@users.noreply.github.com>
2026-02-02 15:43:47 +0900

    [Silver III] Title: 체스판 다시 칠하기, Time: 76 ms, Memory: 32544 KB -BaekjoonHub

--- 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
@@ -1,45 +1,27 @@
-from collections import deque
-import copy
+n, m = map(int, input().split())
+board = [input() for _ in range(n)]
 
-n, m = map(int,input().split())
-
-checkBoard = []
-direct = [[0,1],[0,-1],[1,0],[-1,0]]
 min_res = float('inf')
 
-for _ in range(n):
-    checkBoard.append(list(input()))
+for i in range(n - 7):
+    for j in range(m - 7):
+        cnt1 = 0  # 시작이 W
+        cnt2 = 0  # 시작이 B
 
-def bfs(a,b,ch):
-    res = 0
-    check = copy.deepcopy(checkBoard)
-    if check[a][b] != ch:
-        check[a][b] = ch
-        res += 1
-    queue = deque([(a,b)])
-    visited = [[False]*m for _ in range(n)]
-    visited[0][0] = True
-    while queue:
-        x, y = queue.popleft()
-        for nx, ny in direct:
-            dx = x + nx
-            dy = y + ny
-            if a <= dx < a+8 and b <= dy < b+8:
-                if not visited[dx][dy]:
-                    visited[dx][dy] = True
-                    if check[x][y] == check[dx][dy]:
-                        if check[dx][dy] == "B":
-                            check[dx][dy] = "W"  
-                        else:
-                            check[dx][dy] = "B"
-                        res += 1
-                    queue.append((dx,dy))
-    return res 
+        for x in range(8):
+            for y in range(8):
+                cur = board[i + x][j + y]
+                if (x + y) % 2 == 0:
+                    if cur != 'W':
+                        cnt1 += 1
+                    if cur != 'B':
+                        cnt2 += 1
+                else:
+                    if cur != 'B':
+                        cnt1 += 1
+                    if cur != 'W':
+                        cnt2 += 1
 
-for i in range(n-7):
-    for j in range(m-7):
-        res1 = bfs(i,j,"W")
-        res2 = bfs(i,j,"B")
-        min_res = min(min_res,res1,res2)
+        min_res = min(min_res, cnt1, cnt2)
 
 print(min_res)
