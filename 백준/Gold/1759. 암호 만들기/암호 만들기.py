bedc7a027ce4e8630c7fd92342cbe8e9d4e2b7e7
Leegaeun1 <140165122+Leegaeun1@users.noreply.github.com>
2026-03-05 10:30:12 +0900

    [Gold V] Title: 암호 만들기, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/1759. 암호 만들기/암호 만들기.py
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.py
@@ -1,34 +1,20 @@
-l, c = map(int,input().split())
-alp = list(input().split())
-alp.sort()
-tmp = []
-v = ['a','e','i','o','u']
+l, c = map(int, input().split())
+alp = sorted(input().split())
 
-visited = [False] * len(alp)
+vowels = {'a', 'e', 'i', 'o', 'u'}
 
-def dfs(li):
-    if len(li) == l:
-        tmp.append(li)
-        #print(li)
+def dfs(start, path):
+    if len(path) == l:
+        v_cnt = 0
+        for ch in path:
+            if ch in vowels:
+                v_cnt += 1
+        
+        if v_cnt >= 1 and l - v_cnt >= 2:
+            print(''.join(path))
         return
-    
-    for a in range(len(alp)):
-        if not visited[a]:
-            visited[a] = True
-            if len(li) == 0 or len(li) > 0 and li[-1] <= alp[a]:
-                dfs(li+[alp[a]])
-            visited[a] = False
 
-dfs([])
+    for i in range(start, c):
+        dfs(i + 1, path + [alp[i]])
 
-
-for l in tmp:
-    v_cnt = 0
-    w_cnt = 0
-    for i in l:
-        if i in v:
-            v_cnt += 1
-        else:
-            w_cnt += 1
-    if v_cnt >= 1 and w_cnt >=2:
-        print(''.join(l))
+dfs(0, [])
