131c807540da95e837ed9ebc8d35874b01e34a45
Leegaeun1 <140165122+Leegaeun1@users.noreply.github.com>
2026-03-22 20:19:53 +0900

    [Gold I] Title: 구슬 탈출 2, Time: 64 ms, Memory: 35036 KB -BaekjoonHub

--- 백준/Gold/13460. 구슬 탈출 2/구슬 탈출 2.py
+++ 백준/Gold/13460. 구슬 탈출 2/구슬 탈출 2.py
@@ -1,88 +1,69 @@
 from collections import deque
+
 n, m = map(int,input().split())
 board = []
-direct = [[0,1],[0,-1],[1,0],[-1,0]]
+direct = [(0,1),(0,-1),(1,0),(-1,0)]
 
+def roll(x, y, dx, dy):
+    cnt = 0
+    while board[x+dx][y+dy] != '#':
+        x += dx
+        y += dy
+        cnt += 1
+        if [x,y] == hole:
+            break
+    return x, y, cnt
 
-def isrange(x, y):
-    if 0 <= x < n and 0 <= y < m and board[x][y] !='#':
-        return True
-    return False
-    
-def move(dx,dy,r_r,r_c,b_r,b_c):
-    prev = [r_r,r_c,b_r,b_c]
-    isfin = False
-    blue_hole = False
-    while True:
-        red_move = True
-        blue_move = True
-        r_r += dx
-        r_c += dy
-        if isrange(r_r, r_c)==False or [r_r,r_c] == [b_r,b_c]: # 움직이지 못함
-            r_r -= dx
-            r_c -= dy
-            red_move = False
-        elif hole == [r_r,r_c]:
-            isfin = True
-            r_r += dx
-            r_c += dy
-            
-        
-        b_r += dx
-        b_c += dy
-        if isrange(b_r, b_c)==False or [r_r,r_c] == [b_r,b_c]:# 움직이지 못함
-            b_r -= dx
-            b_c -= dy
-            blue_move = False
-        elif hole == [b_r,b_c]:
-            blue_hole = True
-            break
-        
-        if not blue_move and not red_move: # 못 움직임
-            break
-        if prev == [r_r,r_c,b_r,b_c]:
-            break
-        prev = [r_r,r_c,b_r,b_c]
-    return r_r,r_c,b_r,b_c,isfin,blue_hole
-
-def main():
-    queue = deque([(red[0],red[1],blue[0],blue[1],0)])
+def bfs():
+    queue = deque()
+    queue.append((red[0], red[1], blue[0], blue[1], 0))
     visited = set()
-    min_res = float('inf')
-    
+    visited.add((red[0], red[1], blue[0], blue[1]))
+
     while queue:
-        r_r,r_c,b_r,b_c,cnt = queue.popleft()
-        upper = False
-        
-        if cnt > 10: # 10 이상이면 x
-            if min_res == float('inf') or min_res == 11:
-                upper = True
-            break
-        if (r_r,r_c,b_r,b_c) not in visited:
-            for dx, dy in direct: # 상하좌우
-                if (isrange(r_r+dx, r_c+dy) and board[r_r+dx][r_c+dy] != '#') or (isrange(b_r+dx, b_c+dy) and board[b_r+dx][b_c+dy] !='#'):
-                    rr,rc,br,bc,isfin,blue_hole = move(dx,dy,r_r,r_c,b_r,b_c)
-                    #print(rr,rc,br,bc,isfin,cnt+1,dx,dy)
-                    
-                    if isfin and not blue_hole:
-                        min_res = min(min_res,cnt+1)
-                    if not blue_hole:
-                        queue.append([rr,rc,br,bc,cnt+1])
-            visited.add((r_r,r_c,b_r,b_c))
-    if min_res > 0 and min_res != float('inf') and not upper:
-        print(min_res)
-    else:
-        print(-1)
-    
+        r_r, r_c, b_r, b_c, cnt = queue.popleft()
+
+        if cnt >= 10:
+            continue
+
+        for dx, dy in direct:
+            nr_r, nr_c, r_cnt = roll(r_r, r_c, dx, dy)
+            nb_r, nb_c, b_cnt = roll(b_r, b_c, dx, dy)
+
+            # 파란 구슬 먼저 빠지면 실패
+            if [nb_r, nb_c] == hole:
+                continue
+
+            # 빨간 구슬만 빠지면 성공
+            if [nr_r, nr_c] == hole:
+                return cnt + 1
+
+            # 같은 위치면 조정
+            if nr_r == nb_r and nr_c == nb_c:
+                if r_cnt > b_cnt:
+                    nr_r -= dx
+                    nr_c -= dy
+                else:
+                    nb_r -= dx
+                    nb_c -= dy
+
+            if (nr_r, nr_c, nb_r, nb_c) not in visited:
+                visited.add((nr_r, nr_c, nb_r, nb_c))
+                queue.append((nr_r, nr_c, nb_r, nb_c, cnt+1))
+
+    return -1
+
+
+# 입력
 for i in range(n):
     tmp = list(input())
     for j in range(m):
-        if tmp[j] == 'B':
-            blue = [i,j]
-        elif tmp[j] == 'R':
+        if tmp[j] == 'R':
             red = [i,j]
+        elif tmp[j] == 'B':
+            blue = [i,j]
         elif tmp[j] == 'O':
             hole = [i,j]
     board.append(tmp)
 
-main()
+print(bfs())
