d22acccb3f838c3d4210c73de766ce6823c71e14
Leegaeun1 <140165122+Leegaeun1@users.noreply.github.com>
2026-05-12 18:15:59 +0900

    [level 3] Title: 가장 먼 노드, Time: 23.46 ms, Memory: 19.6 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.py
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.py
@@ -1,26 +1,28 @@
-from collections import defaultdict,deque
+from collections import defaultdict, deque
 
 def solution(n, edge):
-    answer = 0
-    
-    depth = defaultdict(int)
-    depth[1] = 1
-    
     nodes = defaultdict(list)
+
+    # 그래프 저장
     for a, b in edge:
         nodes[a].append(b)
         nodes[b].append(a)
-    
-    visited = [False]*(n+1)
-    visited[1] = True
-    queue = deque([(1,1)]) # 노드번호, depth
-    
+
+    # 각 노드까지 거리 저장
+    depth = [-1] * (n + 1)
+    depth[1] = 0
+
+    queue = deque([1])
+
+    # BFS
     while queue:
-        node, dep = queue.popleft()
-        
+        node = queue.popleft()
+
         for x in nodes[node]:
-            if not visited[x]:
-                queue.append((x,dep+1))
-                depth[dep+1] += 1
-                visited[x] = True
-    return depth[len(depth)]
+            if depth[x] == -1:   # 방문 안 한 경우
+                depth[x] = depth[node] + 1
+                queue.append(x)
+
+    max_depth = max(depth)
+
+    return depth.count(max_depth)
