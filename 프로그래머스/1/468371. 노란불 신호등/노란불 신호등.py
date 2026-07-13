d074acc8fd419296bf61528f5184c5bd765d3269
Leegaeun1 <140165122+Leegaeun1@users.noreply.github.com>
2026-07-13 13:39:30 +0900

    [level 1] Title: 노란불 신호등, Time: 1339.79 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/1/468371. 노란불 신호등/노란불 신호등.py
+++ 프로그래머스/1/468371. 노란불 신호등/노란불 신호등.py
@@ -1,24 +1,20 @@
 def solution(signals):
-    answer = 0
-    y_time = [] # 노란불 시간 [[시작,끝+1]]
-    times = []
-    period = [sum(s) for s in signals] # 각각 주기 
-    length = len(signals)
-    for r, y, g in signals:
-        y_time.append([r+1,r+y+1])
-        
-    dictionary = {}
-    
-    for i in range(100000):
-        for j in range(length):
-            start, end = y_time[j]
-            for s in range(start, end):
-                tmp = s+period[j]*i
-                if tmp not in dictionary:
-                    dictionary[tmp] = 1
-                else:
-                    dictionary[tmp] += 1
-                if dictionary[tmp] == length:
-                    return tmp
-            
+    t = 0
+
+    while t <= 10000000:
+
+        ok = True
+
+        for r,y,g in signals:
+            p = r+y+g
+            mod = t % p
+
+            if not (r < mod <= r+y):
+                ok = False
+                break
+
+        if ok:
+            return t
+
+        t += 1
     return -1
